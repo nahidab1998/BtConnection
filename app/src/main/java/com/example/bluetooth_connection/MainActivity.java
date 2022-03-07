d419fdead1a4cc9bdf8289a9a34c9fe7f6976220
nahid.ab <n.alibagheri1998@gmail.com>
2022-03-07 17:53:55 +0330

    fix error

--- app/src/main/java/com/example/bluetooth_connection/MainActivity.java
+++ app/src/main/java/com/example/bluetooth_connection/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView listen , send , listDevices ;
     private ListView listView;
-    private TextView msg_box , status;
+    private TextView msg_box , status , not_found;
     private EditText writeMsg ;
     private String string ,st_message;
 
@@ -68,176 +68,28 @@ public class MainActivity extends AppCompatActivity {
 
         implementListeners();
 
-    }
-
-    private void implementListeners() {
-
-        listDevices.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Set<BluetoothDevice> bt=bluetoothAdapter.getBondedDevices();
-                String[] strings=new String[bt.size()];
-                btArray=new BluetoothDevice[bt.size()];
-                int index=0;
-
-                if( bt.size()>0)
-                {
-                    for(BluetoothDevice device : bt)
-                    {
-                        btArray[index]= device;
-                        strings[index]=device.getName();
-                        index++;
-                    }
-                    ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,strings);
-                    listView.setAdapter(arrayAdapter);
-                }
-            }
-        });
-
-        listen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ServerClass serverClass=new ServerClass();
-                serverClass.start();
-            }
-        });
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                ClientClass clientClass=new ClientClass(btArray[i]);
-                clientClass.start();
-
-                status.setText("Connecting");
-            }
-        });
-
-        send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                st_message = writeMsg.getText().toString();
-
-                if(TextUtils.isEmpty(st_message)){
-
-                    Toast.makeText(MainActivity.this, "پیغام خود را بنویسید", Toast.LENGTH_SHORT).show();
-
-                }else {
-                    string= String.valueOf(writeMsg.getText());
-                    sendReceive.write(string.getBytes());
-                }
-            }
-        });
 
     }
 
-    Handler handler=new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
+    private void listDevice() {
 
-            switch (msg.what)
+        Set<BluetoothDevice> bt=bluetoothAdapter.getBondedDevices();
+        String[] strings=new String[bt.size()];
+        btArray=new BluetoothDevice[bt.size()];
+        int index=0;
+
+        if( bt.size()>0)
+        {
+            not_found.setVisibility(View.GONE);
+
+            for(BluetoothDevice device : bt)
             {
-                case STATE_LISTENING:
-                    status.setText("Listening");
-                    break;
-                case STATE_CONNECTING:
-                    status.setText("Connecting");
-                    break;
-                case STATE_CONNECTED:
-                    status.setText("Connected");
-//                    status.setTextColor();
-                    break;
-                case STATE_CONNECTION_FAILED:
-                    status.setText("Connection Failed");
-                    break;
-                case STATE_MESSAGE_RECEIVED:
-                    byte[] readBuff= (byte[]) msg.obj;
-                    String tempMsg=new String(readBuff,0,msg.arg1);
-                    msg_box.setText(tempMsg);
-                    break;
+                btArray[index]= device;
+                strings[index]=device.getName();
+                index++;
             }
-            return true;
-        }
-    });
-
-    private class ServerClass extends Thread
-    {
-        private BluetoothServerSocket serverSocket;
-
-        public ServerClass(){
-            try {
-                serverSocket=bluetoothAdapter.listenUsingRfcommWithServiceRecord(APP_NAME,MY_UUID);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void run()
-        {
-            BluetoothSocket socket=null;
-
-            while (socket==null)
-            {
-                try {
-                    Message message=Message.obtain();
-                    message.what=STATE_CONNECTING;
-                    handler.sendMessage(message);
-
-                    socket=serverSocket.accept();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Message message=Message.obtain();
-                    message.what=STATE_CONNECTION_FAILED;
-                    handler.sendMessage(message);
-                }
-
-                if(socket!=null)
-                {
-                    Message message=Message.obtain();
-                    message.what=STATE_CONNECTED;
-                    handler.sendMessage(message);
-
-//                    sendReceive=new SendReceive(socket);
-//                    sendReceive.start();
-                    break;
-                }
-            }
-        }
-    }
-
-    private class ClientClass extends Thread
-    {
-        private BluetoothDevice device;
-        private BluetoothSocket socket;
-
-        public ClientClass (BluetoothDevice device1)
-        {
-            device=device1;
-
-            try {
-                socket=device.createRfcommSocketToServiceRecord(MY_UUID);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void run()
-        {
-            try {
-                socket.connect();
-                Message message=Message.obtain();
-                message.what=STATE_CONNECTED;
-                handler.sendMessage(message);
-
-                sendReceive=new SendReceive(socket);
-                sendReceive.start();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                Message message=Message.obtain();
-                message.what=STATE_CONNECTION_FAILED;
-                handler.sendMessage(message);
-            }
+            ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,strings);
+            listView.setAdapter(arrayAdapter);
         }
     }
 
@@ -290,13 +142,200 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void implementListeners() {
+
+//        listDevices.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Set<BluetoothDevice> bt=bluetoothAdapter.getBondedDevices();
+//                String[] strings=new String[bt.size()];
+//                btArray=new BluetoothDevice[bt.size()];
+//                int index=0;
+//
+//                if( bt.size()>0)
+//                {
+//                    for(BluetoothDevice device : bt)
+//                    {
+//                        btArray[index]= device;
+//                        strings[index]=device.getName();
+//                        index++;
+//                    }
+//                    ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,strings);
+//                    listView.setAdapter(arrayAdapter);
+//                }
+//            }
+//        });
+
+        listen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ServerClass serverClass=new ServerClass();
+                serverClass.start();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ClientClass clientClass=new ClientClass(btArray[i]);
+                clientClass.start();
+
+                status.setText("Connecting");
+            }
+        });
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                st_message = writeMsg.getText().toString();
+
+                if(TextUtils.isEmpty(st_message)){
+
+                    Toast.makeText(MainActivity.this, "پیغام خود را بنویسید", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    string= String.valueOf(writeMsg.getText());
+                    sendReceive.write(string.getBytes());
+                    writeMsg.setText("");
+                    Toast.makeText(MainActivity.this, "ارسال شد", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    Handler handler=new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+
+            switch (msg.what)
+            {
+                case STATE_LISTENING:
+                    status.setText("Listening...");
+                    break;
+                case STATE_CONNECTING:
+                    status.setText("Connecting...");
+                    break;
+                case STATE_CONNECTED:
+                    status.setText("Connected");
+//                    if (status.getText().toString() == "Connected"){
+//
+//                        status.setTextColor(getResources().getColor(R.color.connected));
+//                    }
+                    break;
+                case STATE_CONNECTION_FAILED:
+                    status.setText("Connection Failed");
+//                    status.setTextColor(getResources().getColor(R.color.connection_failed));
+                    break;
+                case STATE_MESSAGE_RECEIVED:
+                    byte[] readBuff= (byte[]) msg.obj;
+                    String tempMsg=new String(readBuff,0,msg.arg1);
+                    msg_box.setText(tempMsg);
+                    break;
+            }
+            return true;
+        }
+    });
+
+    private class ServerClass extends Thread
+    {
+        private BluetoothServerSocket serverSocket;
+
+        public ServerClass(){
+            try {
+                serverSocket=bluetoothAdapter.listenUsingRfcommWithServiceRecord(APP_NAME,MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            BluetoothSocket socket=null;
+
+            while (socket==null)
+            {
+                try {
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTING;
+                    handler.sendMessage(message);
+
+                    socket=serverSocket.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTION_FAILED;
+                    handler.sendMessage(message);
+                }
+
+                if(socket!=null)
+                {
+                    Message message=Message.obtain();
+                    message.what=STATE_CONNECTED;
+                    handler.sendMessage(message);
+
+                    sendReceive=new SendReceive(socket);
+                    sendReceive.start();
+                    break;
+                }
+            }
+        }
+    }
+
+    private class ClientClass extends Thread
+    {
+        private BluetoothDevice device;
+        private BluetoothSocket socket;
+
+        public ClientClass (BluetoothDevice device1)
+        {
+            device=device1;
+
+            try {
+                socket=device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            try {
+                socket.connect();
+                Message message=Message.obtain();
+                message.what=STATE_CONNECTED;
+                handler.sendMessage(message);
+
+                sendReceive=new SendReceive(socket);
+                sendReceive.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Message message=Message.obtain();
+                message.what=STATE_CONNECTION_FAILED;
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+
+
+
     private void initId() {
         listen = findViewById(R.id.bt_listen);
         send = findViewById(R.id.bt_send);
-        listDevices = findViewById(R.id.bt_listdevice);
+//        listDevices = findViewById(R.id.bt_listdevice);
         listView = findViewById(R.id.listview_device);
         msg_box = findViewById(R.id.txt_message);
         status = findViewById(R.id.txt_status);
         writeMsg = findViewById(R.id.editxt_message);
+        not_found = findViewById(R.id.txt_not_found);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        listDevice();
     }
 }
